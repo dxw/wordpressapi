02c94b7d5d0ee1228a09cf0c04cad29e75f37ab1
Tom Adams <tom@holizz.com>
2010-04-26 16:38:39 +0100

    Add a Doxyfile.

--- doxydoc.rb
+++ doxydoc.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'rdoc' # ensure we're using the gem, not the stdlib
 require 'rdoc/rdoc'
 require 'rdoc/parser'
+require 'tempfile'
 
 class RDoc::Parser::Doxygen < RDoc::Parser
 
@@ -13,12 +14,27 @@ class RDoc::Parser::Doxygen < RDoc::Parser
     super
 
     @path = file_name
+    generate_doxyfile
   end
 
   def scan
     # method = RDoc::AnyMethod.new(nil, 'the_method')
     # @top_level.add_method method
     @top_level
+  end
+
+  def generate_doxyfile
+    @doxyfile = Tempfile.open('Doxyfile')
+    @doxyout = nil
+    Tempfile.open('doxygen') do |f|
+      @doxyout=f.path
+      f.close
+    end
+
+    input_files = %Q%"#{@path}"%
+    xml_output = %Q%"#{@doxyout}"%
+    @doxyfile.write ERB.new(open(File.join(File.dirname(__FILE__),'Doxyfile.erb')).read).result(binding)
+    @doxyfile.flush
   end
 
 end
