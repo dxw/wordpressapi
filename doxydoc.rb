cbb8710c1c8c4689ec3a7ef0e24845d570545cd2
Tom Adams <tom@holizz.com>
2010-04-26 16:19:33 +0100

    Find the path.

--- doxydoc.rb
+++ doxydoc.rb
@@ -12,17 +12,12 @@ class RDoc::Parser::Doxygen < RDoc::Parser
   def initialize(top_level, file_name, content, options, stats)
     super
 
-    preprocess = RDoc::Markup::PreProcess.new @file_name, @options.rdoc_include
-
-    preprocess.handle @content do |directive, param|
-      top_level.metadata[directive] = param
-      false
-    end
+    @path = file_name
   end
 
   def scan
-    p 'scanning'
-    p @top_level
+    # method = RDoc::AnyMethod.new(nil, 'the_method')
+    # @top_level.add_method method
     @top_level
   end
 
@@ -30,5 +25,5 @@ end
 
 if __FILE__ == $0
   r = RDoc::RDoc.new
-  r.document(%w[category.php -o output])
+  r.document %w[wp-includes -o output -f darkfish]
 end
